be2e5bceb938c4f8f04939faf93c47ebfa227589
smickael <mickael.swaminathan@w6d.io>
2021-07-30 11:35:00 +0200

    Deploy Next.js to gh-pages

---
+++ out/_next/static/chunks/d64684d8-963379041663e22b0298.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(t,n,r){"use strict";r.d(n,{T7S:function(){return o}});var e=r(4405);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M17 8l4 4m0 0l-4 4m4-4H3"}}]})(t)}}}]);

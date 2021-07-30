ac7cf89c53ec59d68d219ff4898600d013b85217
smickael <mickael.swaminathan@w6d.io>
2021-07-30 11:31:58 +0200

    Env modifs

--- next.config.js
+++ next.config.js
@@ -7,3 +7,12 @@ const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
   assetPrefix: !debug ? '/Next-gh-page-example/' : '',
 }
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  assetPrefix: !debug ? '/developer-portal/' : '',
+}

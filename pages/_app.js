0abdb5c01e9122b18a600e3f2ce8cb5345383b1a
smickael <mickael.k.swaminathan@gmail.com>
2021-08-04 12:52:25 +0200

    Updates SEO meta tags for main page

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,28 @@
-import "@duik/it/dist/styles.css";
-import "../styles/globals.scss";
-import { AppProps } from "next/app";
-import Helmet from "react-helmet";
+import '@duik/it/dist/styles.css'
+import '../styles/globals.scss'
+import { AppProps } from 'next/app'
+import Helmet from 'react-helmet'
 
 function App({ Component, pageProps }, AppProps) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof window === "undefined" ? null : (
-        <>
-          <Helmet>
-            <html lang="en" />
-            <meta
-              name="description"
-              content="Wildcard developer operations - automated DevOps for your business."
-            />
-            <title>Blog - Wildcard Portal</title>
-          </Helmet>
-          <Component {...pageProps} />
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div suppressHydrationWarning>
+            {typeof window === 'undefined' ? null : (
+                <>
+                    <Helmet>
+                        <html lang="en" />
+                        <meta
+                            name="description"
+                            content="The best no-code continuous integration platform for your business devOps. Build your visual pipeline and integrate third party apps into your builds and deployment."
+                        />
+                        <title>
+                            Wildcard Continuous Integration CI/CD Platform Blog
+                        </title>
+                    </Helmet>
+                    <Component {...pageProps} />
+                </>
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App

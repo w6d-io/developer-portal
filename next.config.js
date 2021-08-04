babfffc1d0b83a034156e986978e648ebcb59185
smickael <mickael.k.swaminathan@gmail.com>
2021-08-04 12:47:21 +0200

    Adds blog to exportPathMap

--- next.config.js
+++ next.config.js
@@ -2,21 +2,21 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // assetPrefix: isProd ? 'https://developer-portal.w6d.io' : '',
-  assetPrefix: isProd ? "http://localhost:3000" : "",
-};
+    // assetPrefix: isProd ? 'https://developer-portal.w6d.io' : '',
+    assetPrefix: isProd ? 'http://localhost:3000' : '',
+}
 
 module.exports = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/blog": { page: "/blog" },
-    };
-  },
-};
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/blog': { page: '/blog' },
+        }
+    },
+}

60eb6fa7419f6d2a96ec4523b700339b91a6a385
smickael <mickael.k.swaminathan@gmail.com>
2021-08-04 11:36:01 +0200

    Prepares next.config for deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // assetPrefix: isProd ? 'https://developer-portal.w6d.io' : '',
-  assetPrefix: isProd ? 'http://localhost:3000' : '',
-
+  assetPrefix: isProd ? 'https://developer-portal.w6d.io' : '',
+  // assetPrefix: isProd ? 'http://localhost:3000' : '',
 }

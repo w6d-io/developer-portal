343e39e51ef612b9dbe5348fd9697c7d1d306b6e
smickael <mickael.k.swaminathan@gmail.com>
2021-08-04 12:49:38 +0200

    Moves blog index into directory

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { ContainerVertical } from '@duik/it'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './Home'
-import Blog from './Blog'
+import Blog from './blog'
 /* Blog as Homepage START */
 import fs from 'fs'
 import path from 'path'
@@ -12,37 +12,40 @@ import { sortByDate } from '../utils'
 import Link from 'next/link'
 /* Blog as Homepage END */
 
-export default function Layout({posts}) {
-  return (
-    <div className="home-container">
-      <Header/>
-      <ContainerVertical>
-        <Blog posts={posts} />
-      </ContainerVertical>
-    </div>
-  )
+export default function Layout({ posts }) {
+    return (
+        <div className="home-container">
+            <Header />
+            <ContainerVertical>
+                <Blog posts={posts} />
+            </ContainerVertical>
+        </div>
+    )
 }
 
 /* Blog as Homepage START */
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-  const posts = files.map(filename => {
-      const slug = filename.replace('.md', '')
-      const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-      const { data: frontmatter } = matter(markdownWithMeta)
+    const files = fs.readdirSync(path.join('posts'))
+    const posts = files.map((filename) => {
+        const slug = filename.replace('.md', '')
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', filename),
+            'utf-8'
+        )
+        const { data: frontmatter } = matter(markdownWithMeta)
 
-      return {
-          slug,
-          frontmatter,
-      }
-  })
+        return {
+            slug,
+            frontmatter,
+        }
+    })
 
-  return {
-      props: {
-          posts: posts.sort(sortByDate)
-      }
-  }
+    return {
+        props: {
+            posts: posts.sort(sortByDate),
+        },
+    }
 }
 
 /* Blog as Homepage END */
